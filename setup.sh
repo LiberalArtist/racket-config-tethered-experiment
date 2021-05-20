e6e85c60981e02269807150a021548955672fce6
Philip McGrath <philip@philipmcgrath.com>
2021-05-20 15:25:06 -0400

    start to reorganize main-distribution test

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/gnu/store/7vy0ikhd3ws5v6jydzx7aipvs4jv7rb9-racket-minimal-8.1/bin/racket \
-    --config /home/philip/code/tmp/racket-config-tethered/etc/racket/ \
-    -l raco -- setup --no-user
